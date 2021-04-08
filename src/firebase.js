9928a08bd37744f8368036ea1d8e89d79d01b593
web2033 <github@web2033.com>
2021-04-08 09:44:37 +0300

    prod DB

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseApp = initializeApp({
-  databaseURL: null,
+  databaseURL: 'https://rapid-api-1-default-rtdb.firebaseio.com',
 })
 
 const db = getDatabase(firebaseApp)
 if (location.hostname === 'localhost') {
   // Point to the RTDB emulator running on localhost.
-  db.useEmulator('localhost', 9000)
+  // db.useEmulator('localhost', 9000)
 }
 
 const FBdata = db
